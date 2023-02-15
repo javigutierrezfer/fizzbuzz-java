021596fb7dcc4bc027a34e63784f400055070104
Javier Gutierrez <javier.gutierrez@540deg.com>
2023-02-15 18:38:21 +0100

    Configuration

--- src/main/java/com/deg540/fizzbuzz/FizzBuzz.java
+++ src/main/java/com/deg540/fizzbuzz/FizzBuzz.java
@@ -1,4 +1,8 @@
 package com.deg540.fizzbuzz;
 
 class FizzBuzz {
+
+    public String convert(int number) {
+        throw new UnsupportedOperationException("implement me");
+    }
 }
--- src/test/java/com/deg540/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/com/deg540/fizzbuzz/FizzBuzzTest.java
@@ -1,12 +1,14 @@
 package com.deg540.fizzbuzz;
 
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class FizzBuzzTest {
     @Test
-    public void firstTest() {
-        assertTrue(true);
+    public void convert_1_to_1() {
+        assertEquals("1", new FizzBuzz().convert(1));
     }
 }
 
