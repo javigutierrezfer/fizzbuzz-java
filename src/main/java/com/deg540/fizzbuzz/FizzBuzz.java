eecd209e06e994528de49006e9ea25302e76b408
Javier Gutierrez <javier.gutierrez@540deg.com>
2023-02-16 16:23:37 +0100

    [SOL] Añadir 3 casos base

--- src/main/java/com/deg540/fizzbuzz/FizzBuzz.java
+++ src/main/java/com/deg540/fizzbuzz/FizzBuzz.java
@@ -3,6 +3,14 @@ package com.deg540.fizzbuzz;
 class FizzBuzz {
 
     public String convert(int number) {
-        throw new UnsupportedOperationException("implement me");
+        if (number ==  1) {
+            return "1";
+        }
+
+        if (number == 2) {
+            return "2";
+        }
+
+        return "4";
     }
 }
--- src/test/java/com/deg540/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/com/deg540/fizzbuzz/FizzBuzzTest.java
@@ -3,12 +3,23 @@ package com.deg540.fizzbuzz;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class FizzBuzzTest {
+    FizzBuzz fizzBuzz = new FizzBuzz();
+
     @Test
     public void convert_1_to_1() {
-        assertEquals("1", new FizzBuzz().convert(1));
+        assertEquals("1", fizzBuzz.convert(1));
+    }
+
+    @Test
+    public void convert_2_to_2() {
+        assertEquals("2", fizzBuzz.convert(2));
+    }
+
+    @Test
+    public void convert_4_to_4() {
+        assertEquals("4", fizzBuzz.convert(4));
     }
 }
 
